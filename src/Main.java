3bbdeb08aabef008d3538023f0c9125113f04f82
abdelhalim khaldi <golden-boy1994@hotmail.fr>
2019-04-21 23:01:54 -0700

    added new stuff

--- src/Main.java
+++ src/Main.java
@@ -72,32 +72,61 @@ public class Main {
     }
 
     public void readContent(Path filePath) throws IOException {
-        String[] possibleValues = {"(a) Chi^2", "(c) Chi^2", "(a) The maximum partial sum", "(d) d"};
+        String[] possibleValues = {"(a) Chi^2",
+                "(h) Chi^2", "(c) Chi^2",
+                "(a) The maximum partial sum",
+                "(d) d",
+                "(b) S_n/n",
+                "C0   C1   C2   C3   C4   C5   C6    CHI2",
+                "0      1      2      3      4    >=5   Chi^2",
+                "(b) V_n_obs (Total # of runs)",
+                "(f) Del_1",
+                "(g) Del_2",
+                "(i) discarded"};
         System.out.println("file name " + filePath.getFileName());
         FileWriter outFile = null;
         List<String> fileList = Files.readAllLines(filePath);
-        for(String possible : possibleValues) {
-            int j=0;
-            outFile = new FileWriter(possible+".txt",true);
+        for (String possible : possibleValues) {
+            int j = 0;
+            String name = possible;
+            if(possible.equalsIgnoreCase("(b) S_n/n")){
+                name="(b) sn";
+            }else if(possible.equalsIgnoreCase("0      1      2      3      4    >=5   Chi^2")){
+                name="Chi^2";
+            }
+            int index=0;
+            outFile = new FileWriter(name + ".txt", true);
             for (String x : fileList) {
+                System.out.println(x);
                 String[] splited = x.split("\t");
                 for (String a : splited) {
                     if (a.indexOf(possible) != -1) {
-                        j++;
-                        String[] splited1 = a.split("=");
-                        outFile.write(splited1[1] + "\r\n");
+                        if(possible.equalsIgnoreCase("C0   C1   C2   C3   C4   C5   C6    CHI2") || possible.equalsIgnoreCase("0      1      2      3      4    >=5   Chi^2"))
+                        {
+                            String[] splited1 = fileList.get(index+2).split("\\s");
+                            outFile.write(splited1[splited1.length-1] + "\r\n");
+
+
+                        }else {
+                            j++;
+                            String[] splited1 = a.split("=");
+                            outFile.write(splited1[1] + "\r\n");
+                        }
 //                    i++;
 //                    System.out.println(a);
                     }
                 }
 
+
 //        System.out.println("" + fileList);
+
+//            System.out.println("this is the data to check in each line  "+possible+" and  number of it  : "+j);
+        index++;
             }
-            System.out.println("this is the data to check in each line  "+possible+" and  number of it  : "+j);
+
             outFile.close();
+
         }
-
-
     }
 }
 
